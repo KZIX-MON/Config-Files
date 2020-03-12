6498f7a8a2735dea2a8cc663db5d2ee755d90b8b
KZIX-MON <61951573+KZIX-MON@users.noreply.github.com>
2020-03-12 05:59:00 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,7 @@
 
 #This program is supposed to Auto Install the programs and Dotfiles that I need
 
-pacman -S --noconfirm git
-pacman -S --noconfirm i3
-pacman -S --noconfirm zsh
-pacman -S --noconfirm nvim
-pacman -S --noconfirm zathura
-pacman -S --noconfirm zathura-pdf-mupdf
+sudo pacman -S --noconfirm git i3 zsh neovim zathura zathura-pdf-mupdf
 
 mkdir ~/.git
 cd ~
@@ -16,11 +11,11 @@ cd ~
 cd ~/.git
 git clone https://git.suckless.org/st
 cd st
-make clean install
+sudo make clean install
 cd ~/.git
 git clone https://git.suckless.org/dmenu
 cd dmenu
-make clean install
+sudo make clean install
 cd ~/.git
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -28,10 +23,10 @@ makepkg -si
 cd
 
 #Yay installs
-yay -S wd719x
-yay -S aic94xx
-yay -S picom-git
-yay -S brave-bin
+yay -S --noconfirm wd719x-firmware
+yay -S --noconfirm aic94xx-firmware
+yay -S --noconfirm picom-git
+yay -S --noconfirm brave-bin
 
 chsh -s /bin/zsh $USER
 
@@ -49,8 +44,8 @@ mv ~/Config-Files/frankfurt.jpeg ~/pics || echo "Not Found..."
 mv ~/Config-Files/tech.jpeg ~/pics || echo "Not Found..."
 
 
-mv ~/Config-Files/config/zsh/.zshrc ~
-mv ~/Config-Files/config/i3/config ~/.config/i3
-mv ~/Config-Files/config/i3blocks/config ~/.config/i3blocks
-mv ~/Config-Files/config/picom ~/.config/picom
-mv ~/Config-Files/config/neofetch ~/.config/neofetch
+mv ~/Config-Files/configs/zsh/.zshrc ~
+mv ~/Config-Files/configs/i3/config ~/.config/i3
+mv ~/Config-Files/configs/i3blocks/config ~/.config/i3blocks
+mv ~/Config-Files/configs/picom ~/.config/picom
+mv ~/Config-Files/configs/neofetch ~/.config/neofetch
