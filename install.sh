4b76ebd6fee8b289bed7e0be509e197e63fe4148
KZIX-MON <61951573+KZIX-MON@users.noreply.github.com>
2020-03-13 12:49:35 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,20 +2,21 @@
 
 #This program is supposed to Auto Install the programs and Dotfiles that I need
 
-sudo pacman -S --noconfirm git i3 zsh neovim zathura zathura-pdf-mupdf
+#Updates Pacman
+sudo pacman -Syu --noconfirm
 
+#Installs all needed programs
+sudo pacman -S --noconfirm i3 
+sudo pacman -S --noconfirm zsh
+sudo pacman -S --noconfirm neovim
+sudo pacman -S --noconfirm zathura
+sudo pacman -S --noconfirm zathura-pdf-mupdf
+
+#Initializes git step
 mkdir ~/.git
 cd ~
 
 #Git Installs
-cd ~/.git
-git clone https://git.suckless.org/st
-cd st
-sudo make clean install
-cd ~/.git
-git clone https://git.suckless.org/dmenu
-cd dmenu
-sudo make clean install
 cd ~/.git
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -44,8 +45,18 @@ mv ~/Config-Files/frankfurt.jpeg ~/pics || echo "Not Found..."
 mv ~/Config-Files/tech.jpeg ~/pics || echo "Not Found..."
 
 
-mv ~/Config-Files/configs/zsh/.zshrc ~
-mv ~/Config-Files/configs/i3/config ~/.config/i3
-mv ~/Config-Files/configs/i3blocks/config ~/.config/i3blocks
-mv ~/Config-Files/configs/picom ~/.config/picom
-mv ~/Config-Files/configs/neofetch ~/.config/neofetch
+mv ~/Config-Files/configs/zsh/.zshrc ~ || echo "Not moved"
+mv ~/Config-Files/configs/i3/config ~/.config/i3 || echo "Not moved"
+mv ~/Config-Files/configs/i3blocks/config ~/.config/i3blocks || echo "Not moved"
+mv ~/Config-Files/configs/picom ~/.config/picom || echo "Not moved"
+mv ~/Config-Files/configs/neofetch ~/.config/neofetch || echo "Not moved"
+
+#Installs Suckless Programs
+cd ~/.git
+git clone https://git.suckless.org/st
+cd st
+sudo make install
+cd ~/.git
+git clone https://git.suckless.org/dmenu
+cd dmenu
+sudo make install
